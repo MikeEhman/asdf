a4d09c1d59d5ae8cfa23ce5918a0f5a76861017a
nkim412@gmail.com <nkim412@gmail.com>
2013-05-22 14:41:54 -0500

    fdsa

--- build.bat
+++ build.bat
@@ -1,4 +1,7 @@
 C:\Chutzpah\chutzpah.console.exe test.js /teamcity
-cd ~/.ssh
 call ssh -T git@github.com
+call remote add origin https://github.com/MikeEhman/asdf.git
+call git init
+call git add .
+call git commit -m "asdf"
 call git push origin master
