eb76e269341af0bdca9d15e8fea44f57eda8f300
nkim412@gmail.com <nkim412@gmail.com>
2013-05-22 16:10:43 -0500

    test19

--- build.bat
+++ build.bat
@@ -2,6 +2,3 @@ C:\Chutzpah\chutzpah.console.exe test.js /teamcity
 call git config --global user.email nkim412@gmail.com
 call git config --global user.name "Nam Hee Kim"
 call git branch
-call git checkout master
-call git merge --no-ff develop
-call git push origin master
