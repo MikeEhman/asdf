861a8daee50368df4e13b47143f56000685b1949
nkim412@gmail.com <nkim412@gmail.com>
2013-05-22 14:09:32 -0500

    test2

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 test("test1", function(){
 	var testVal = 1;
 	equal(1, testVal, "testVal is equal to 1");
-	equal(2, testVal, "testVal is equal to 2");
+	equal(1, testVal, "testVal is equal to 1");
 });
