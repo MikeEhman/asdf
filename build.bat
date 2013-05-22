2c48629d242d064b1acbea1e2a4afe649d0e3b04
nkim412@gmail.com <nkim412@gmail.com>
2013-05-22 15:32:13 -0500

    test11

--- build.bat
+++ build.bat
@@ -1,3 +1,7 @@
 C:\Chutzpah\chutzpah.console.exe test.js /teamcity
 call git pull origin master
 call git status
+call git add .
+call git status
+call git commit -m "teamcity"
+call git push origin master
