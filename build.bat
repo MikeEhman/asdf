1e95a0b47081539acebcb457e03eb10aac3a37f5
MikeEhman <nkim412@gmail.com>
2013-05-23 13:47:07 -0500

    test41

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 C:\Chutzpah\chutzpah.console.exe test.js /teamcity
 call git branch master
 call git checkout master
-call git pull master
+call git pull origin master
 call git merge --no-ff develop
 call git commit -m "Processed"
 call git push origin master
